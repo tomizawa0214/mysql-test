534be25d160406619f512f63756d7a8816ae1d1a
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-05-19 18:01:43 +0900

    商品追加の注文処理

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+
+from django.conf import settings
 from django.db import models
 
 class Item(models.Model):
@@ -10,3 +12,31 @@ class Item(models.Model):
 
   def __str__(self):
     return self.title
+
+class OrderItem(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  ordered = models.BooleanField(default=False)
+  item = models.ForeignKey(Item, on_delete=models.CASCADE)
+  quantity = models.IntegerField(default=1)
+
+  def get_total_item_price(self):
+    return self.quantity * self.item.price
+
+  def __str__(self):
+    return f"{self.item.title} : {self.quantity}"
+
+class Order(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  items = models.ManyToManyField(OrderItem)
+  start_data = models.DateTimeField(auto_now_add=True)
+  ordered_data = models.DateTimeField()
+  ordered = models.BooleanField(default=False)
+
+  def get_total(self):
+    total = 0
+    for order_item in self.items.all():
+      total += order_item.get_total_item_price()
+    return total
+
+  def __str__(self):
+    return self.user.email
--- app/views.py
+++ app/views.py
@@ -1,10 +1,36 @@
-from django.views.generic import ListView, DetailView
-from .models import Item
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView, View
+from .models import Item, OrderItem, Order
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+@login_required
+def addItem(request, slug):
+  item = get_object_or_404(Item, slug=slug)
+  order_item, created = OrderItem.objects.get_or_create(
+    item=item,
+    user=request.user,
+    ordered=False
+  )
+  order = Order.objects.filter(user=request.user, ordered=False)
+
+  if order.exists():
+    order = order[0]
+    if order.items.filter(item__slug=item.slug).exists():
+      order_item.quantity += 1
+      order_item.save()
+    else:
+      order.items.add(order_item)
+  else:
+    order = Order.objects.create(user=request.user, ordered_data=timezone.now())
+    prder.items.add(order_item)
+
+  return redirect('order')
 
 class ItemDetailView(DetailView):
   model = Item
   template_name = 'app/product.html'
-  
+
 class IndexView(ListView):
   model = Item
   template_name = 'app/index.html'
