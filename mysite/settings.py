81c6eb86958578677eabf575faf00c0892e246ef
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-05-20 18:18:44 +0900

    設定変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,6 +90,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 IMAGE_URL = '/images/'
 IMAGE_ROOT = os.path.join(BASE_DIR, 'images')
 
